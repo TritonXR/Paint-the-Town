8472af30deddee43b32866ca01e57fd5e9e38687
inviswings <inviswings@gmail.com>
2018-02-05 11:21:40 -0800

    adding Material

---
+++ Paint the Town v1/Assets/Materials and Shaders/RaycastShootComplete.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastShootComplete : MonoBehaviour {
+
+	public float weaponRange = 50f;                                     // Distance in Unity units over which the player can fire
+	public Transform gunEnd;                                            // Holds a reference to the gun end object, marking the muzzle location of the gun
+                                            //increase the color of 
+	private Camera fpsCam;                                              // Holds a reference to the first person camera
+
+	void Start () 
+	{
+
+		// Get and store a reference to our Camera by searching this GameObject and its parents
+		fpsCam = GetComponentInParent<Camera>();
+	}
+
+	void Update () 
+	{
+		// Check if the player has pressed the fire button and if enough time has elapsed since they last fired
+		if (Input.GetButton("Fire1"))// && Time.time > nextFire) 
+		{
+
+			// Create a vector at the center of our camera's viewport
+			Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
+
+			// Declare a raycast hit to store information about what our raycast has hit
+			RaycastHit hit;
+
+			// Check if our raycast has hit anything
+			if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+			{
+				Material mat;
+				//red play
+				if (this.tag == "PlayerRed") {
+					mat = hit.collider.GetComponent<Renderer>().material;
+					if (mat != null) {
+						
+						//Texture2D redTex = (mat.GetTexture("_Red") as Texture2D);
+						Texture2D redTex = (Texture2D)GameObject.Instantiate(mat.GetTexture("_Red"));
+						//Debug.Log ("name:"+redTex.name);
+						Vector2 pixelUV = hit.textureCoord;
+						pixelUV.x *= redTex.width;
+						pixelUV.y *= redTex.height;
+						int x =(int) pixelUV.x;
+						int y = 0;
+						//radius of the brush size
+						int radius = 2;
+						x = radius - 1;
+						int dx = 1;
+						int dy = 1;
+						int err = dx - (radius << 1);
+						while (x >= y) {
+						redTex.SetPixel ((int)pixelUV.x+x, (int)pixelUV.y+y, Color.white);
+							redTex.SetPixel ((int)pixelUV.x+y, (int)pixelUV.y+x, Color.white);
+							redTex.SetPixel ((int)pixelUV.x-y, (int)pixelUV.y+x, Color.white);
+							redTex.SetPixel ((int)pixelUV.x-x, (int)pixelUV.y+y, Color.white);
+							redTex.SetPixel ((int)pixelUV.x-x, (int)pixelUV.y-y, Color.white);
+							redTex.SetPixel ((int)pixelUV.x-y, (int)pixelUV.y-x, Color.white);
+							redTex.SetPixel ((int)pixelUV.x+y, (int)pixelUV.y-x, Color.white);
+							redTex.SetPixel ((int)pixelUV.x+x, (int)pixelUV.y-y, Color.white);
+							redTex.SetPixel ((int)pixelUV.x, (int)pixelUV.y, Color.white);
+							if (err <= 0) {
+								y++;
+								err += dy;
+								dy += 2;
+							}
+							if (err > 0) {
+								x--;
+								dx += 2;
+								err += dx - (radius << 1);
+							}
+						}
+
+						//redTex.Apply ();
+						MeshRenderer render = GetComponent<MeshRenderer> ();
+						render.sharedMaterials [0].mainTexture = redTex;
+						mat.SetTexture("_Red", redTex);
+						//mat.color = Color.green;
+					}
+				}
+				//green play
+				else if (this.tag == "PlayerGreen") {
+					mat = hit.collider.GetComponent<Renderer>().material;
+					if (mat != null) {
+						mat.SetFloat ("_Green", 1.0f);
+						//	mat.color = Color.green;
+					}
+				}
+				//Blue play
+				else if (this.tag == "PlayerBlue") {
+					mat = hit.collider.GetComponent<Renderer>().material;
+					if (mat != null) {
+						mat.SetFloat ("_Blue", 1.0f);
+						//	mat.color = Color.green;
+					}
+				}
+					
+
+			}
+			else
+			{}
+		}
+	}
+}
---
+++ Paint the Town v1/Assets/Materials and Shaders/TestingShader.shader
@@ -0,0 +1,125 @@
+﻿Shader "Custom/TestingShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		//_Red ("Red", Range(0,1)) = 0.0
+		//_Green ("Green", Range(0,1)) = 0.0
+		//_Blue ("Blue", Range(0,1)) = 0.0
+
+		_Red("Red", 2D) = "black" {}
+		_Green("Green", 2D) = "black2" {}
+		_Blue("Blue", 2D) = "black3" {}
+	}
+	SubShader
+	{
+
+		Pass
+		{
+		    Tags{"lighMode" = "ForwardBase"}
+            
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#pragma multi_compile_fwdbase
+			#include "AutoLight.cginc"
+			#include "Lighting.cginc"
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 pos : SV_POSITION;
+
+				SHADOW_COORDS(1)
+				fixed3 diff : COLOR0;
+				fixed3 ambient : COLOR1;
+
+			};
+
+			v2f vert (appdata_base v)
+			{
+				v2f o;
+
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = v.texcoord;
+
+				half3 worldNormal = UnityObjectToWorldNormal(v.normal);
+				half n1 = max(0, dot(worldNormal, _WorldSpaceLightPos0.xyz));
+				o.diff = n1 * _LightColor0.rgb;
+				o.ambient = ShadeSH9(half4(worldNormal, 1));
+				TRANSFER_SHADOW(o);
+
+				//TRANSFER_VERTEX_TO_FRAGMENT(o);
+				return o;
+			}
+			
+			sampler2D _MainTex;
+
+			//float _Red;
+			//float _Green;
+			//float _Blue;
+
+			sampler2D _Red;
+			sampler2D _Green;
+			sampler2D _Blue;
+
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				fixed4 daRed = tex2D(_Red, i.uv);
+				fixed4 daBlue = tex2D(_Blue, i.uv);
+				fixed4 daGreen = tex2D(_Green, i.uv);
+
+				//float sumrgb = (_Red + _Green + _Blue) / 3.0;
+				//float4 white = float4(1.0, 1.0, 1.0, 1.0);
+				//col = (1 - sumrgb) * white + (sumrgb * float4(_Red * col.r, _Green * col.g, _Blue * col.b, col.a));
+
+				float sumrgb = (daRed.r + daGreen.g + daBlue.b) / 3.0;
+				float4 white = float4(1.0, 1.0, 1.0, 1.0);
+				col = (1 - sumrgb) * white + (sumrgb * float4(daRed.r * col.r,  daGreen.g * col.g, daBlue.b * col.b, col.a));
+
+				//shadow stuffs
+				fixed shadow = SHADOW_ATTENUATION(i);
+				fixed lighting = i.diff * shadow + i.ambient;
+				col.rgb *= lighting+0.5f;
+
+
+				return col;
+			}
+			ENDCG
+		}
+
+
+		Pass {
+			Name "ShadowCaster"
+			Tags { "LightMode" = "ShadowCaster" }
+			
+			ZWrite On ZTest LEqual
+
+			CGPROGRAM
+			#pragma target 2.0
+
+			#pragma shader_feature _ _ALPHATEST_ON _ALPHABLEND_ON _ALPHAPREMULTIPLY_ON
+			#pragma shader_feature _METALLICGLOSSMAP
+			#pragma skip_variants SHADOWS_SOFT
+			#pragma multi_compile_shadowcaster
+
+			#pragma vertex vertShadowCaster
+			#pragma fragment fragShadowCaster
+
+			#include "UnityStandardShadow.cginc"
+
+			ENDCG
+		}
+	}
+	//FALLBACK "VertexLit"
+}
