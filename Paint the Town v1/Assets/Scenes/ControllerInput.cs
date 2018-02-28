a52d82f6ac4d95175f78add77a69240af199b4e0
jacobUgalde <jugalde413@gmail.com>
2018-02-27 16:38:25 -0800

    raycast comes out of pointed finger input
    Play is an example scene where, if an Oculus with controllers are connected, then pulling the lower right-hand trigger and pointing the index finger will shoot a raycast, which in this case destroys the provided box

---
+++ Paint the Town v1/Assets/Scenes/ControllerInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour
+{
+    public Transform controllerTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (OVRInput.Touch.(OVRInput.Button.PrimaryHandTrigger))
+        if(!OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger) && !OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
+        {
+            Debug.Log("touched");
+
+            RaycastHit hit;
+
+            if(Physics.Raycast(controllerTransform.position, controllerTransform.forward, out hit))
+            {
+                if(hit.collider.gameObject.CompareTag("Cube"))
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+	}
+
+   
+}
