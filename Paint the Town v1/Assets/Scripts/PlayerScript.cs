59ceb5e94df1f05cc99801c7f79c3a85266a0a47
Gregory J Morgan <gjmorgan@UCSD.EDU>
2018-03-12 13:17:03 -0700

    prototype scene functioning, YAAAS

--- Paint the Town v1/Assets/Scripts/PlayerScript.cs
+++ Paint the Town v1/Assets/Scripts/PlayerScript.cs
@@ -22,8 +22,10 @@ public class PlayerScript : NetworkBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-        if (!isLocalPlayer) {
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
             OVRInput.Update();
             test = OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote).ToString();
             Vector2 moveInput = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
@@ -41,18 +43,7 @@ public class PlayerScript : NetworkBehaviour {
                 Physics.Raycast()
             } */
         }
-        
-	}
 
-    Vector3 SampleCurve(Vector3 start, Vector3 end, Vector3 control, float t)
-    {
-        // Interpolate along line S0: control - start;
-        Vector3 Q0 = Vector3.Lerp(start, control, t);
-        // Interpolate along line S1: S1 = end - control;
-        Vector3 Q1 = Vector3.Lerp(control, end, t);
-        // Interpolate along line S2: Q1 - Q0
-        Vector3 Q2 = Vector3.Lerp(Q0, Q1, t);
-
-      return Q2; // Q2 is a point on the curve at time t
     }
+
 }
---
+++ Paint the Town v1/Assets/Scripts/movementthing.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementthing : MonoBehaviour {
+
+    public string test;
+
+    GameObject player;
+    public Camera headsetCamera;
+    public GameObject lefthand;
+    public GameObject righthand;
+    // Use this for initialization
+    void Start()
+    {
+        player = this.gameObject;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        OVRInput.Update();
+        test = OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote).ToString();
+        Vector2 rightInput = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        Vector2 leftInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        
+        Vector3 newMove = Vector3.zero;
+
+        if (rightInput.y > 0.3 || leftInput.y > 0.3)
+            newMove += headsetCamera.transform.forward;
+
+        if (rightInput.y < -0.3 || leftInput.y < -0.3)
+            newMove += -headsetCamera.transform.forward;
+
+        if (rightInput.x > 0.3 || leftInput.x > 0.3)
+            newMove += headsetCamera.transform.right;
+
+        if (rightInput.x < -0.3 || leftInput.x < -0.3)
+            newMove += -headsetCamera.transform.right;
+
+        newMove.y = 0;
+
+        player.transform.Translate(newMove * Time.deltaTime * 1f);
+
+
+        /*if (OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger))
+        {
+            Vector3 start = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+            Vector3 ray = SampleCurve(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger)), 
+            Physics.Raycast()
+        } */
+
+
+    }
+}
