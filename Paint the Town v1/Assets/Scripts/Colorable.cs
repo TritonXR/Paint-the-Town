8106d5c9fdb72bc75a0a14a28cdcbc116ea5e1cd
Jacob M Ugalde <jmugalde@UCSD.EDU>
2018-04-19 13:33:53 -0700

    started working on shader affecting other components like sound, also need to fix material on hand models

--- Paint the Town v1/Assets/Scripts/Colorable.cs
+++ Paint the Town v1/Assets/Scripts/Colorable.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Colorable : MonoBehaviour {
 
+    Material mat;
+    Renderer ren;
+    float scale;
+    AudioSource sound;
+
 	// Use this for initialization
 	void Start () {
-		
+        mat = GetComponent<Material>();
+        ren = GetComponent<Renderer>();
+        scale = 0.0f;
+        sound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //manipulate scale here
+        //but you still have to get the _Transition variable in order to change volume and everything, right?
+        ren.material.SetFloat("_Transition", scale);
+        //sound.volume
+    }
 }
