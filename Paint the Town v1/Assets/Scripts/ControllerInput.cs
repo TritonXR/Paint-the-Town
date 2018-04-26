a6883d165ba46ba283f599e45834c37c63376210
Jacob M Ugalde <jmugalde@UCSD.EDU>
2018-04-26 13:23:49 -0700

    UI interaction is finally working (also happy birthday greg)

--- Paint the Town v1/Assets/Scripts/ControllerInput.cs
+++ Paint the Town v1/Assets/Scripts/ControllerInput.cs
@@ -2,6 +2,155 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//TODO: upon creating rooms/characters, have each InputManager object have this script attached, but each object has a different tag
+//troubleshooting tips: make sure whatever is being colored has the Colorable script attached to it; make sure InputManager is tagged as PlayerRed, PlayerBlue, or PlayerGreen
+//make sure InputManager has ControllerInput.cs attached to it; make sure colored object's material is using the shader Custom/TestingShader
+//to get hand models, just add the LocalAvatar Prefab from Assets/OvrAvatar/Content/Prefabs
+
+public class ControllerInput : MonoBehaviour
+{
+    public Transform controllerTransform;
+    public GameObject lefthand;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO add a line to visualize the raycast
+
+        //if (OVRInput.Touch.(OVRInput.Button.PrimaryHandTrigger))
+        if ((!OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger) && !OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.Get(OVRInput.Button.PrimaryHandTrigger)) ||
+            (!OVRInput.Get(OVRInput.Touch.SecondaryIndexTrigger) && !OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) && OVRInput.Get(OVRInput.Button.SecondaryHandTrigger)))
+        {
+            //Debug.Log("touched");
+
+            RaycastHit hit;
+            Material mat;
+
+            if (Physics.Raycast(controllerTransform.position, controllerTransform.forward, out hit) ||
+                Physics.Raycast(lefthand.transform.position, lefthand.transform.forward, out hit))
+            {
+
+                // red player
+                if (this.tag == "PlayerRed")
+                {
+                    if (hit.collider.GetComponent<UIClick>() != null)
+                    {
+                        Debug.Log("clicked");
+                        //if(hit.collider.name == "Button")
+                        //{
+                        //do whatever
+
+                        Debug.Log(hit.collider.name);
+                        Button uiButton = hit.collider.GetComponent<Button>();
+                        Color r = Color.red;
+                        ColorBlock cb = uiButton.colors;
+                        cb.highlightedColor = r;
+                        uiButton.colors = cb;
+
+                        //the line below changes the current scene to the apartment scene
+                        if (hit.collider.name == "CreateGameButton")
+                            SceneManager.LoadScene("Prototype Scene - PreMaster");
+                        //and so on with if statements
+                        if (hit.collider.name == "SettingsButton")
+                        {
+                            //show settings
+                            //uiButton.onClick();
+                            hit.collider.GetComponentInParent<MainMenu>().gameObject.SetActive(false);
+                        }
+
+                        //Quit the game when user hits quit from the menu
+                        if (hit.collider.name == "Quit")
+                            Application.Quit();
+                    }
+
+                    // checking the raycast hit a paintable target
+                    if (hit.collider.GetComponent<Colorable>() != null)
+                    {
+                        mat = hit.collider.GetComponent<Renderer>().material;
+
+                        // create a new texture to paint on
+                        Texture2D redTex = GameObject.Instantiate(mat.GetTexture("_Red")) as Texture2D;
+
+                        // paint the whole thing red
+                        for (int x = 0; x < redTex.width; x++)
+                        {
+                            for (int y = 0; y < redTex.height; y++)
+                            {
+                                redTex.SetPixel(x, y, Color.white);
+                            }
+                        }
+                        redTex.Apply();
+                        mat.SetTexture("_Red", redTex);
+                    }
+                }
+                //green player
+                else if (this.tag == "PlayerGreen")
+                {
+                    if (hit.collider.GetComponent<Colorable>() != null)
+                    {
+                        mat = hit.collider.GetComponent<Renderer>().material;
+
+                        // create a new texture to paint on
+                        Texture2D tex = GameObject.Instantiate(mat.GetTexture("_Green")) as Texture2D;
+
+                        // paint the whole thing red
+                        for (int x = 0; x < tex.width; x++)
+                        {
+                            for (int y = 0; y < tex.height; y++)
+                            {
+                                tex.SetPixel(x, y, Color.white);
+                            }
+                        }
+                        tex.Apply();
+                        mat.SetTexture("_Green", tex);
+                    }
+                }
+                //Blue play
+                else if (this.tag == "PlayerBlue")
+                {
+                    if (hit.collider.GetComponent<Colorable>() != null)
+                    {
+                        mat = hit.collider.GetComponent<Renderer>().material;
+
+                        // create a new texture to paint on
+                        Texture2D tex = GameObject.Instantiate(mat.GetTexture("_Blue")) as Texture2D;
+
+                        // paint the whole thing red
+                        for (int x = 0; x < tex.width; x++)
+                        {
+                            for (int y = 0; y < tex.height; y++)
+                            {
+                                tex.SetPixel(x, y, Color.white);
+                            }
+                        }
+                        tex.Apply();
+                        mat.SetTexture("_Blue", tex);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+
+
+
+//i'm sorry
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Linq;
 
 
@@ -46,6 +195,7 @@ public class ControllerInput : MonoBehaviour
             (!OVRInput.Get(OVRInput.Touch.SecondaryIndexTrigger) && !OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) && OVRInput.Get(OVRInput.Button.SecondaryHandTrigger)))
         {
             color_change();
+            uiInteract();
         }
         else
         {
@@ -63,6 +213,56 @@ public class ControllerInput : MonoBehaviour
         //}
     }
 
+    void uiInteract()
+    {
+        // Create a vector at the center of our camera's viewport
+        rayOrigin = gunEnd.transform.position;
+
+        // Declare a raycast hit to store information about what our raycast has hit
+        RaycastHit hit;
+
+        // Check if our raycast has hit anything
+        if (Physics.Raycast(rayOrigin, gunEnd.transform.forward, out hit, weaponRange))
+        {
+            Material mat;
+
+            ///NEW STUFF
+
+            // check if last hit was the on the same texture 
+            hitCurr = true;
+
+            Debug.Log("clicked");
+            //if(hit.collider.name == "Button")
+            //{
+            //do whatever
+
+            Debug.Log(hit.collider.name);
+            Button uiButton = hit.collider.GetComponent<Button>();
+            Color r = Color.red;
+            ColorBlock cb = uiButton.colors;
+            cb.highlightedColor = r;
+            uiButton.colors = cb;
+
+            //the line below changes the current scene to the apartment scene
+            if (hit.collider.name == "CreateGameButton")
+                SceneManager.LoadScene("Prototype Scene - PreMaster");
+            //and so on with if statements
+            if (hit.collider.name == "SettingsButton")
+            {
+                //show settings
+                //uiButton.onClick();
+                hit.collider.GetComponentInParent<MainMenu>().gameObject.SetActive(false);
+            }
+
+            //Quit the game when user hits quit from the menu
+            if (hit.collider.name == "Quit")
+                Application.Quit();
+
+            ///END NEW STUFF
+        }
+    }
+
+
     void color_change()
     {
 
@@ -77,12 +277,9 @@ public class ControllerInput : MonoBehaviour
         {
             Material mat;
 
-            // check if last hit was the on the same texture 
-            hitCurr = true;
-
-            // red player
-            if (this.tag == "PlayerRed")
-            {
+        // red player
+        if (this.tag == "PlayerRed")
+        {
                 // checking the raycast hit a paintable target
                 if (hit.collider.GetComponent<Colorable>() != null)
                 {
@@ -131,10 +328,10 @@ public class ControllerInput : MonoBehaviour
                         hitLast = true;
                     }
                 }
-            }
-            //green play
-            else if (this.tag == "PlayerGreen")
-            {
+        }
+        //green play
+        else if (this.tag == "PlayerGreen")
+         {
                 if (hit.collider.GetComponent<Colorable>() != null)
                 {
                     mat = hit.collider.GetComponent<Renderer>().material;
@@ -240,3 +437,4 @@ public class ControllerInput : MonoBehaviour
 
    
 }
+*/
