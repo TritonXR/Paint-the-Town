ac6e21adf11e139d9d7554135fdfcdc58221dfc4
Bryan H Thai <b3thai@UCSD.EDU>
2018-06-08 02:22:57 -0700

    doggo done do a color

--- Paint the Town v1/Assets/Scripts/StatueScript.cs
+++ Paint the Town v1/Assets/Scripts/StatueScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class StatueScript : MonoBehaviour {
 
+    public GameObject statueBase;
     private Colorable statueColorable;
+    private Colorable statueBaseColorable;
 	// Use this for initialization
 	void Start () {
         statueColorable = GetComponent<Colorable>();
+        statueBaseColorable = statueBase.GetComponent<Colorable>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (statueColorable.curState == state.D)
+        if (statueColorable.curState == state.D && statueBaseColorable.curState == state.D)
         {
             if (PhotonNetwork.isMasterClient)
             {
